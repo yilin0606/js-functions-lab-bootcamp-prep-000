986eecfd32dd35a6cf978cf7ad462e870105a09c
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-10 09:26:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function happyHolidaysTo(name){
  return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo()
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday},${name}!`
+}
+
+ 
